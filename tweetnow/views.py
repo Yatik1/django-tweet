41d88fcb54fa00cd2545ca798ca2ae24da7761c3
Yatik Srivastava <yatiksrivastava1@gmail.com>
2024-07-22 12:36:51 +0530

    get

--- tweetnow/views.py
+++ tweetnow/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from .models import Tweet
+from .forms import TweetForm
+from django.shortcuts import get_object_or_404 , redirect
 
-# Create your views here.
 
 def index(request):
     return render(request , 'index.html')
+
+def tweet_list(request):
+    tweets = Tweet.objects.all().order_by('-created_at')
+    return render(request,'tweet_list.html',{'tweets' : tweets})
+
