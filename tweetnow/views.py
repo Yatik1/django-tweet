daf85a7553e0209d4a2ac697e92450d111a92f4d
Yatik Srivastava <yatiksrivastava1@gmail.com>
2024-07-24 13:31:35 +0530

    view debug

--- tweetnow/views.py
+++ tweetnow/views.py
@@ -8,7 +8,7 @@ def index(request):
     return render(request , 'index.html')
 
 def tweet_list(request):
-    tweets = Tweet.objects.all().order_by('-created_at')
+    tweets = Tweet.objects.all().order_by('-created_At')
     return render(request,'tweet_list.html',{'tweets' : tweets})
 
 def tweet_create(request):
@@ -26,7 +26,7 @@ def tweet_create(request):
 
 
 def tweet_edit(request , tweet_id):
-    tweet = get_object_or_404(Tweet , pl=tweet_id,user = request.user)
+    tweet = get_object_or_404(Tweet , pk=tweet_id,user = request.user)
     if request.method == "POST":
         form = TweetForm(request.POST , request.FILES ,instance=tweet)
         if form.is_valid():
